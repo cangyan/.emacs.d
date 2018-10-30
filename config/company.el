d342ed5b2464a102120b7ad6bb4c7d975f44dd0a
Mingfeng Wang <me.wmf@foxmail.com>
2018-10-30 15:37:21 +0800

    :sparkles: company mode

---
+++ config/company.el
@@ -0,0 +1,31 @@
+(use-package company
+  :ensure t
+  :config
+  (global-company-mode t)
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 3)
+  (setq company-backends
+        '((company-files
+           company-yasnippet
+           company-keywords
+           company-capf
+           )
+          (company-abbrev company-dabbrev))))
+
+(add-hook 'emacs-lisp-mode-hook
+            (lambda ()
+              (add-to-list (make-local-variable 'company-backends) 
+              '(company-elisp))))
+
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous))
+
+
+(advice-add 'company-complete-common :before (lambda () 
+                                (setq my-company-point (point))))
+(advice-add 'company-complete-common :after (lambda ()
+                                (when (equal my-company-point (point))
+                                                  (yas-expand))))
